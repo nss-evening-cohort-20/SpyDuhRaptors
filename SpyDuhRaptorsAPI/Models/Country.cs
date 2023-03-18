48dd380e3b8a2151bf12a242ed3d6083c79867b7
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-03-18 13:03:20 -0500

    Created classes for User, Country, Agency, and Assignment. I have also updated the SQL script.

---
+++ SpyDuhRaptorsAPI/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace SpyDuhRaptorsAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public string Region { get; set; }
+    }
+}
