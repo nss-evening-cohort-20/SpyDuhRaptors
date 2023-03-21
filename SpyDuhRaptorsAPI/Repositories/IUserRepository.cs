da25ec813510f70f3bd76e9a97bab8368b8768c9
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-03-21 14:47:27 -0500

    Completed Repos for User, countrym Agency, and Assignment. I have also extracted interfaces for each one.

---
+++ SpyDuhRaptorsAPI/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SpyDuhRaptorsAPI.Models;
+
+namespace SpyDuhRaptorsAPI.Repositories
+{
+    public interface IUserRepository
+    {
+        void Add(User variety);
+        void Delete(int id);
+        User Get(int id);
+        List<User> GetAll();
+        void Update(User variety);
+    }
+}
