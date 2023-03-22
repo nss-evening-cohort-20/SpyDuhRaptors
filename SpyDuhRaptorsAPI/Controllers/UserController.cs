8503f76ae40195f7b39dd831c7ee6dad3074be76
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-03-21 19:42:38 -0500

    created a UserDto model and updated the getall and get by id methods from User repository

---
+++ SpyDuhRaptorsAPI/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpyDuhRaptorsAPI.Models;
+using SpyDuhRaptorsAPI.Repositories;
+
+namespace SpyDuhRaptorsAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
