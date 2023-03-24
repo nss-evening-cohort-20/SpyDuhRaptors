26afe698ef26fe923799578e218a0f01fb872f47
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-03-24 14:31:55 -0500

    Completed the controllers for User, Country, Assignment, and Agency. I have made sure to add the builders in program.cs as well.

--- SpyDuhRaptorsAPI/Program.cs
+++ SpyDuhRaptorsAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using SpyDuhRaptorsAPI.Repositories;
 
 namespace SpyDuhRaptorsAPI
 {
@@ -10,6 +11,10 @@ namespace SpyDuhRaptorsAPI
 
             // Add services to the container.
 
+            builder.Services.AddTransient<IAssignmentRepository, AssignmentRepository>();
+            builder.Services.AddTransient<IAgencyRepository, AgencyRepository>();
+            builder.Services.AddTransient<ICountryRepository, CountryRepository>();
+            builder.Services.AddTransient<IUserRepository, UserRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
